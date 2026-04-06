13e25c6a6e2cd321181c5df4ea04ae885c04a327
Kathirvel Gounder <kathirgounder@gmail.com>
2026-04-05 21:01:03 -0400

    Init File

--- gtsfm/retriever/__init__.py
+++ gtsfm/retriever/__init__.py
@@ -8,17 +8,20 @@
 
 from .exhaustive_retriever import ExhaustiveRetriever
 from .joint_similarity_sequential_retriever import JointSimilaritySequentialRetriever
+from .kmst_retriever import KMstRetriever
 from .sequential_retriever import SequentialRetriever
 from .similarity_retriever import SimilarityRetriever
 
 Exhaustive = ExhaustiveRetriever
 JointSimilaritySequential = JointSimilaritySequentialRetriever
+KMst = KMstRetriever
 Sequential = SequentialRetriever
 Similarity = SimilarityRetriever
 
 __all__ = [
     "Exhaustive",
     "JointSimilaritySequential",
+    "KMst",
     "Sequential",
     "Similarity",
 ]
